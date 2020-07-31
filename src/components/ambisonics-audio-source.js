cfb016ad7c9778509ec084aa944f401bf5e3197b
Thomas Deppisch <tommi_deppisch@gmx.de>
2020-07-31 15:46:51 +0200

    calculate rotation and distance

--- src/components/ambisonics-audio-source.js
+++ src/components/ambisonics-audio-source.js
@@ -164,19 +164,74 @@ export class ambisonicsAudioSource extends THREE.Object3D {
     const lsPosition = new THREE.Vector3();
     const lsQuaternion = new THREE.Quaternion();
     const lsScale = new THREE.Vector3();
-    const lsOrientation = new THREE.Vector3();
+    //const lsOrientation = new THREE.Vector3();
     const avatarPosition = new THREE.Vector3();
+    const avatarLookingDirectionCartesian = new THREE.Vector3();
+    const avatarLookingDirectionSpherical = new THREE.Spherical();
+    const avatarToLoudspeakerDirectionCartesian = new THREE.Vector3();
+    const avatarToLoudspeakerDirectionSpherical = new THREE.Spherical();
+    const encodingDirection = new THREE.Vector3();
+    this.mediaEl.sceneEl.camera.getWorldDirection(avatarLookingDirectionCartesian);
     this.mediaEl.sceneEl.camera.getWorldPosition(avatarPosition);
+    avatarLookingDirectionSpherical.setFromVector3(avatarLookingDirectionCartesian);
 
     for (const ls of this.loudspeakers) {
       ls.matrixWorld.decompose(lsPosition, lsQuaternion, lsScale);
-      lsOrientation.set(0, 0, 1).applyQuaternion(lsQuaternion);
+      avatarToLoudspeakerDirectionCartesian.subVectors(lsPosition, avatarPosition);
+      avatarToLoudspeakerDirectionSpherical.setFromVector3(avatarToLoudspeakerDirectionCartesian);
+      encodingDirection.setFromSphericalCoords(
+        1,
+        avatarToLoudspeakerDirectionSpherical.phi - avatarLookingDirectionSpherical.phi,
+        avatarToLoudspeakerDirectionSpherical.theta - avatarLookingDirectionSpherical.theta
+      );
+
+      // todo: use orientation to apply loudspeaker directivity
+      // lsOrientation.set(0, 0, 1).applyQuaternion(lsQuaternion);
       const distance = avatarPosition.distanceTo(lsPosition);
 
       // use inverse distance law, implementation as in Firefox PannerNode
       const distanceBasedAttenuation =
         this.refDistance /
         (this.refDistance + this.rolloffFactor * (Math.max(distance, this.refDistance) - this.refDistance));
+
+      if (ls === this.loudspeakers[0]) {
+        // console.log("azi: " + 180 / Math.PI * (avatarToLoudspeakerDirectionSpherical.theta - avatarLookingDirectionSpherical.theta));
+        // console.log("zen: " + -180 / Math.PI * (avatarToLoudspeakerDirectionSpherical.phi - avatarLookingDirectionSpherical.phi));
+        console.log(encodingDirection);
+      }
+
+      // Need to update the position on this node if either the listener moves or this node moves,
+      // because otherwise there are audio artifacts in Chrome.
+      // if (
+      //   setInitial ||
+      //   Math.abs(position.x - this._lastPosition.x) > Number.EPSILON ||
+      //   Math.abs(position.y - this._lastPosition.y) > Number.EPSILON ||
+      //   Math.abs(position.z - this._lastPosition.z) > Number.EPSILON ||
+      //   Math.abs(orientation.x - this._lastOrientation.x) > Number.EPSILON ||
+      //   Math.abs(orientation.y - this._lastOrientation.y) > Number.EPSILON ||
+      //   Math.abs(orientation.z - this._lastOrientation.z) > Number.EPSILON ||
+      //   (listener.positionX && Math.abs(listener.positionX.value - this._lastListenerPosition.x) > Number.EPSILON) ||
+      //   (listener.positionY && Math.abs(listener.positionY.value - this._lastListenerPosition.y) > Number.EPSILON) ||
+      //   (listener.positionZ && Math.abs(listener.positionZ.value - this._lastListenerPosition.z) > Number.EPSILON)
+      // ) {
+      //   panner.setPosition(position.x, position.y, position.z);
+      //   panner.setOrientation(orientation.x, orientation.y, orientation.z);
+
+      //   this._lastPosition.x = position.x;
+      //   this._lastPosition.y = position.y;
+      //   this._lastPosition.z = position.z;
+
+      //   this._lastOrientation.x = orientation.x;
+      //   this._lastOrientation.y = orientation.y;
+      //   this._lastOrientation.z = orientation.z;
+
+      //   if (listener.positionX && listener.positionY && listener.positionZ) {
+      //     this._lastListenerPosition.x = listener.positionX.value;
+      //     this._lastListenerPosition.y = listener.positionY.value;
+      //     this._lastListenerPosition.z = listener.positionZ.value;
+      //   }
+      // }
+
     }
   }
 }
